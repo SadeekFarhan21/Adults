81645986500cf8d6eefdc9a013397b761c840704
Farhan Sadeek <farhansadeek019@gmail.com>
2024-05-13 12:42:46 -0400

    Imported the dataset

---
+++ download.py
@@ -0,0 +1,15 @@
+from ucimlrepo import fetch_ucirepo 
+import pandas as pd
+import seaborn as sns
+# fetch dataset 
+adult = fetch_ucirepo(id=2) 
+  
+# data (as pandas dataframes) 
+X = adult.data.features 
+y = adult.data.targets 
+  
+# metadata 
+print(adult.metadata) 
+  
+# variable information 
+print(adult.variables) 
